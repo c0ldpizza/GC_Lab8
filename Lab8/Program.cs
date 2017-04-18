0d28a2c5a161f57c89ff6df184232bb43a1a7838
Charles Lambrecht <lambrechtca@gmail.com>
2017-04-18 14:15:14 -0400

    Initial commit. Continue loop

---
+++ Lab8/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Welcome to the Grand Circus Batting Cage!");
+
+            do
+            {
+                //Main code
+
+            } while (Continue());
+
+            Console.WriteLine("Goodbye!");
+        }
+
+        public static bool Continue()
+        {
+            Console.WriteLine("Another batter? (y/n)");
+
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "n")
+            {
+                return false;
+            }
+            else if (input.ToLower() == "y")
+            {
+                return true;
+            }
+            else
+                Console.WriteLine("Invalid input");
+            Continue();
+            return true;
+            {
+            }
+
+        }
+    }
