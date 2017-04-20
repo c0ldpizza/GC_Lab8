f7c8ec7420a091b130b6552bf350aab55e9442b2
Charles Lambrecht <lambrechtca@gmail.com>
2017-04-20 09:36:57 -0400

    Simplified variables in calculation loop

--- Lab8/Program.cs
+++ Lab8/Program.cs
@@ -39,11 +39,11 @@ namespace Lab8
                 for (int r = 0; r < batters; r++)
                 {
                     decimal x, y, BA, SP = 0;
-                    x = BattingAverage(BatterData[r]);
-                    y = SluggingPercentage(BatterData[r]);
+                    BA = BattingAverage(BatterData[r]);
+                    SP = SluggingPercentage(BatterData[r]);
 
-                    BA = Math.Round(x, 3);
-                    SP = Math.Round(y, 3);
+                    //BA = Math.Round(x, 3);
+                    //SP = Math.Round(y, 3);
 
                     //output results to user
                     Console.WriteLine("Batter {0} Batting average: {1}\t Slugging percentage: {2}", r+1 , BA.ToString("#0.000"), SP.ToString("#0.000"));
